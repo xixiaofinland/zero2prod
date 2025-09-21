b6f90a1f146441c7fe73de6a3f9848bcbc68a480
xi xiao <tdxiaoxi2@gmail.com>
2025-09-21 20:29:01 +0300

    +

---
+++ tests/health_check.rs
@@ -0,0 +1,23 @@
+// `cargo expand --test health_check` (<- name of the test file)
+#[tokio::test]
+async fn health_check_works() {
+    spawn_app();
+
+    let client = reqwest::Client::new();
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to bind address");
+
+    // Launch the server as a background task
+    let _ = tokio::spawn(server);
+}
+
