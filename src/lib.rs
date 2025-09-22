5abc61eb8362aeafb797c4e81e33e7ef26b4c37d
Xi Xiao <xi.xiao007@gmail.com>
2025-09-22 19:40:02 +0300

    add sub endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,28 @@ use std::net::TcpListener;
 
 use actix_web::{App, HttpResponse, HttpServer, dev::Server, web};
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
