b5ddb064b692b42e87d49d581f14f3199fad2f40
Xi Xiao <xi.xiao007@gmail.com>
2025-09-28 09:45:02 +0300

    split into mods

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,4 @@
-use std::net::TcpListener;
+pub mod configuration;
+pub mod routes;
+pub mod startup;
 
-use actix_web::{App, HttpResponse, HttpServer, dev::Server, web};
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-
-    Ok(server)
-}
