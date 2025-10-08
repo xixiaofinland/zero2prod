51b49e0cf1cc8bee2d56cff90987ab2d9918eebf
Xi Xiao <xi.xiao007@gmail.com>
2025-10-08 21:44:05 +0300

    add log and env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
